0b753911fde16f0126ccd45570478880d9b2add3
ajfisher <none@none>
2010-08-14 21:39:18 +1000

    added layout file

---
+++ site_media/js/layout.js
@@ -0,0 +1,19 @@
+/**
+
+This script is used for managing the layout of elements
+
+**/
+
+function doLayout() {
+    // align the form rater to the side of the map
+
+
+    /**$("div#form_rater").position({
+        my: "top",
+        at: "top",
+        of: "#map_holder",
+    });**/
+
+
+
+}
