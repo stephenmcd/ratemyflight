78a999fecfc5f17b38bd4bb902c76a204d3a3acb
stephenmcd <steve@jupo.org>
2010-08-15 05:29:06 +1000

    Initial layout for management command to poll twitter.

---
+++ core/management/commands/poll_twitter.py
@@ -0,0 +1,11 @@
+
+from django.core.management.base import NoArgsCommand
+
+
+class Command(NoArgsCommand):
+    """
+    Polls the Twitter API for ratings with the #ratemyflight hashtag.
+    """
+    def handle_noargs(self, **options):
+        pass
+
