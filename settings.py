b3aac4433fef93686e5861bdde46e003b3a5f619
stephenmcd <steve@jupo.org>
2010-08-14 16:22:47 +1000

    Added some better default settings.

--- settings.py
+++ settings.py
@@ -1,18 +1,24 @@
 # Django settings for ratemyflight project.
 
+import os
+project_path = os.path.dirname(os.path.abspath(__file__))
+project_dir = project_path.split(os.sep)[-1]
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ("Stephen McDonald", "stephen.mc@gmail.com"),
+    ("Andrew Fisher", "ajfisher.td@gmail.com"),
+    ("Josh de Blank", "jdeblank@gmail.com"),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(project_path, "ratemyflight.db"),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,30 +33,30 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = "Australia/Melbourne"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
-USE_L10N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+USE_L10N = False
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/site_media/'
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(project_path, MEDIA_URL.strip("/"))
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -58,7 +64,7 @@ MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '#)a%&0%^@45nl^@cww%$#2c+yf*@=-4)z&lc=&o58du#9+hf-6'
+SECRET_KEY = '#)a%&0%^@45nasdfasdf2c+yf*@=-4)z&lc=&o58du#9+hf-6'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -77,11 +83,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'ratemyflight.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+TEMPLATE_DIRS = (os.path.join(project_path, "templates"),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -89,6 +91,29 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
 )
+
+# Optional apps.
+OPTIONAL_APPS = (
+    "debug_toolbar",
+    "south",
+    "django_extensions", 
+)
+
+import sys
+if not (len(sys.argv) > 1 and sys.argv[1] == "test"):
+    for app in OPTIONAL_APPS:
+        try:
+            __import__(app)
+        except ImportError:
+            pass
+        else:
+            INSTALLED_APPS += (app,)
+INSTALLED_APPS = sorted(list(INSTALLED_APPS), reverse=True)
+
+# Optional app settings.
+if "debug_toolbar" in INSTALLED_APPS:
+    DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
+    MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
