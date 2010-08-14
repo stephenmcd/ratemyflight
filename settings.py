f2d0804f8892b1a237d3d87b953cef435077b21a
stephenmcd <steve@jupo.org>
2010-08-14 17:00:11 +1000

    More 1.2 removal.

--- settings.py
+++ settings.py
@@ -64,15 +64,13 @@ SECRET_KEY = '#)a%&0%^@45nasdfasdf2c+yf*@=-4)z&lc=&o58du#9+hf-6'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    "django.template.loaders.filesystem.load_template_source",
+    "django.template.loaders.app_directories.load_template_source",
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
