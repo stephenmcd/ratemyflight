5c70ef1fba3c3fc6447d90005182736f7a0d117b
stephenmcd <steve@jupo.org>
2010-08-15 08:08:12 +1000

    Added list_display_links for airline and airport admin classes.

--- core/admin.py
+++ core/admin.py
@@ -8,6 +8,8 @@ from core.models import Airline, Airport, Country, Rating
 class AirlineAdmin(admin.ModelAdmin):
     
     list_display = ("name", "callsign", "country", "iata_code", "icao_code",)
+    list_display_links = ("name", "callsign", "country", "iata_code", 
+        "icao_code",)
     list_filter = ("country",)
     search_fields = ("name", "callsign", "country", "iata_code", "icao_code",)
 
@@ -15,6 +17,8 @@ class AirlineAdmin(admin.ModelAdmin):
 class AirportAdmin(admin.ModelAdmin):
     
     list_display = ("name", "city", "country", "iata_code", "icao_code", 
+        "latitude", "longitude",)
+    list_display_links = ("name", "city", "country", "iata_code", "icao_code", 
         "latitude", "longitude",)
     list_filter = ("country",)
     search_fields = ("name", "city", "country", "iata_code", "icao_code",)
@@ -24,7 +28,8 @@ class RatingAdmin(admin.ModelAdmin):
     
     list_display = ("avatar_link", "name", "flight", "airline", 
         "airport_from", "airport_to", "value", "time",)
-    list_display_links = ("name",)
+    list_display_links = ("name", "flight", "airline", 
+        "airport_from", "airport_to", "value", "time",)
     fieldsets = (
         (None, {"fields": (("name", "value", "avatar_url"),)}),
         (_("Flight info"), {"fields": 
