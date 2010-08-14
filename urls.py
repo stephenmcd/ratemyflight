63df23585b8ba45e04e4e5b0e9aef4c2398fcd74
stephenmcd <steve@jupo.org>
2010-08-14 16:36:32 +1000

    Set up structure for homepage.

--- urls.py
+++ urls.py
@@ -1,17 +1,13 @@
+
+from django.conf import settings
 from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^ratemyflight/', include('ratemyflight.foo.urls')),
+admin.autodiscover()
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns("",
+    ("^admin/", include(admin.site.urls)),
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 )
