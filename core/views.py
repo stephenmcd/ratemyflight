411fc0018acabc7e328c7c0c4145589399baeaa1
ajfisher <none@none>
2010-08-15 10:10:03 +1000

    added PM and IDL boundary handling for lookups

--- core/views.py
+++ core/views.py
@@ -9,13 +9,64 @@ def airports_for_boundary(request, south, west, north, east):
     """
     Return a JSON formatted list of airports in the given boundary.
     """
-    try:
-        lookup = {
-            "latitude__gt": float(south), "longitude__gt": float(west),
-            "latitude__lt": float(north), "longitude__lt": float(east),
-        }
-    except ValueError:
-        lookup = {}
-    airports = Airport.objects.filter(**lookup)[:MAX_AIRPORTS]
-    json = serializers.serialize("json", airports)
+    
+    lookup = {}
+    lookup_alt = {}
+    
+    #do a really quick check to determine if we are crossing the PM or IDL
+    if float(west) * float(east) < 0:
+        # we need to do two lookups but we need to determine which side of
+        # which meridian you actually are.
+        
+        #TODO: convert this back to a Q object
+        
+        # this check looks to see if we are closer to the IDL or the PM.
+        # that then determines how we do the split on the two lookups
+        if (180 - float(west)) < float(west) :
+            #we're closest to IDL
+            try:
+                lookup = {
+                    "latitude__gt": float(south), "longitude__gt": float(west),
+                    "latitude__lt": float(north), "longitude__lt": 180,
+                }
+                lookup_alt = {
+                    "latitude__gt": float(south), "longitude__gt": -180,
+                    "latitude__lt": float(north), "longitude__lt": float(east),
+                }   
+                
+            except ValueError:
+                lookup = {}
+                lookup_alt={}
+        else:
+        
+            # closest to PM
+            try:
+                lookup = {
+                    "latitude__gt": float(south), "longitude__gt": float(west),
+                    "latitude__lt": float(north), "longitude__lt": 0,
+                }
+                lookup_alt = {
+                    "latitude__gt": float(south), "longitude__gt": 0,
+                    "latitude__lt": float(north), "longitude__lt": float(east),
+                }   
+                
+            except ValueError:
+                lookup = {}
+                lookup_alt={}
+        
+    else:
+        #we only need to do one lookup in this instance
+    
+        try:
+            lookup = {
+                "latitude__gt": float(south), "longitude__gt": float(west),
+                "latitude__lt": float(north), "longitude__lt": float(east),
+            }
+        except ValueError:
+            lookup = {}
+    
+    airports = Airport.objects.filter(**lookup)
+    airports_alt = Airport.objects.filter(**lookup_alt)
+    
+    json = serializers.serialize("json", (airports | airports_alt)[:MAX_AIRPORTS])
     return HttpResponse(json, mimetype="application/json")
