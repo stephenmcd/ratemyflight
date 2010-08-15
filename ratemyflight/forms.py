e39336ac48462dc4294b30810c0eae6061d2d1f0
stephenmcd <steve@jupo.org>
2010-08-15 11:41:32 +1000

    Defined which fields and widgets exist for the rating form.

--- ratemyflight/forms.py
+++ ratemyflight/forms.py
@@ -6,7 +6,11 @@ from ratemyflight.models import Rating
 
 class RatingForm(forms.ModelForm):
 
+    email = forms.EmailField(_("Email"), required=False, 
+        help_text="Only used to retrieve your gravatar icon.")
+    value = forms.FloatField(max_value=10, min_value=0)
+
     class Meta:
         model = Rating
-        
+        exclude = ("avatar_url", "time", "tweet_id", "tweet_text")
 
