25199434d215dbfce27b7b58cf607bb0e1710838
ajfisher <none@none>
2010-08-15 11:17:47 +1000

    stubbed out flight data api calls for placeholder for web interface

--- ratemyflight/urls.py
+++ ratemyflight/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns("ratemyflight.views",
     url("^airport/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
         "airports_for_boundary", name="airports_for_boundary"),
+    url("^flight/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
+        "flights_for_boundary", name="flights_for_boundary"),
+
 )
 
 
