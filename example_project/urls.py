bbe3bf4b2642f69aaab6c256d3e7f88af9a7d756
stephenmcd <steve@jupo.org>
2010-08-15 11:55:52 +1000

    Move all url patterns into ratemyflight.urls

--- example_project/urls.py
+++ example_project/urls.py
@@ -2,15 +2,13 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
 
 
 admin.autodiscover()
 
 urlpatterns = patterns("",
     ("^admin/", include(admin.site.urls)),
-    url("^api/", include("ratemyflight.urls")),
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+    url("^", include("ratemyflight.urls")),
 )
 
 if getattr(settings, "DEV_SERVER", False):
--- ratemyflight/urls.py
+++ ratemyflight/urls.py
@@ -1,13 +1,14 @@
 
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns("ratemyflight.views",
-    url("^airport/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
+    url("^api/airport/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
         "airports_for_boundary", name="airports_for_boundary"),
-    url("^flight/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
+    url("^api/flight/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
         "flights_for_boundary", name="flights_for_boundary"),
-
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 )
 
 
