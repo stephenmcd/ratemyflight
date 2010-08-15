eb0aeda225cc7c0aef85559857de4cca35b77efd
stephenmcd <steve@jupo.org>
2010-08-15 18:41:25 +1000

    Clean up URLS for API and point final URLS to views.

--- ratemyflight/urls.py
+++ ratemyflight/urls.py
@@ -4,10 +4,15 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns("ratemyflight.views",
-    url("^api/airport/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/$", 
+    url("^api/airport/boundary/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/$", 
         "airports_for_boundary", name="airports_for_boundary"),
-    url("^api/flight/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/$", 
+    url("^api/flight/boundary/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/$", 
         "flights_for_boundary", name="flights_for_boundary"),
+    url("^api/flight/airline/(?P<iata_code>.*)/$", 
+        "flights_for_airline", name="flights_for_airline"),
+    url("^api/flight/username/(?P<username>.*)/$", 
+        "flights_for_username", name="flights_for_username"),
+    url("^api/flight/recent/$", "recent_flights", name="recent_flights"),
     url("^$", "home", name="home"),
 )
 
