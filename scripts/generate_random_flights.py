544bd51c22fb1a0ccf7035eedb1f3c614127c7f3
stephenmcd <steve@jupo.org>
2010-08-15 15:05:15 +1000

    Added script to generate some random flight data for fixtures.

---
+++ scripts/generate_random_flights.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+Create some random flights.
+"""
+
+import os
+import sys
+
+scripts_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(scripts_path, "..")))
+os.environ["DJANGO_SETTINGS_MODULE"] = "example_project.settings"
+
+from random import choice, random, randint
+
+from ratemyflight.models import Rating, Airline, Airport
+
+
+airports = [a for a in Airport.objects.all() if a.iata_code]
+airlines = [a for a in Airline.objects.all() if a.iata_code]
+
+users = (
+    ("stephen_mcd", "http://a3.twimg.com/profile_images/690849843/UNI_2223_normal.jpg"),
+    ("ajfisher", "http://a2.twimg.com/profile_images/93415866/afisher_citrus.com.au_aa97ef53_normal.jpg"),
+    ("joshwins", "http://a0.twimg.com/profile_images/179238148/n820185192_3656151_9483_normal.jpg"),
+)
+
+flights = 100
+
+for i in range(flights):
+    user = choice(users)
+    rating = Rating()
+    rating.name = user[0]
+    rating.avatar_url = user[1]
+    rating.value = random() * 10.
+    rating.airline = choice(airlines)
+    rating.flight = "%s%s" % (rating.airline.iata_code, randint(100, 999))
+    rating.airport_from = choice(airports)
+    while not rating.airport_to or \
+        rating.airport_from.id == rating.airport_to.id:
+        rating.airport_to = choice(airports)
+    rating.comment = "I like turtles!!"
+    rating.save()
+
