8401eaabaf68fa10918604c6039dddcdcae7fd6b
stephenmcd <steve@jupo.org>
2010-08-15 16:16:42 +1000

    Added setting for maximum flights returned via JSON.

--- ratemyflight/settings.py
+++ ratemyflight/settings.py
@@ -1,5 +1,10 @@
 
 
-MAX_AIRPORTS = 100
+# Maximum airports to return as JSON.
+MAX_AIRPORTS = 100 
 
-GRAVATAR_SIZE = 32
+# Maximum flights to return as JSON.
+MAX_FLIGHTS = 100 
+
+# Pixel size of gravatar icons.
+GRAVATAR_SIZE = 32 
