5cad4529bae5da1d4d9efa41571a08566e82b8bb
stephenmcd <steve@jupo.org>
2010-08-15 12:46:18 +1000

    Added view for rating form.

--- ratemyflight/urls.py
+++ ratemyflight/urls.py
@@ -4,9 +4,10 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns("ratemyflight.views",
-    url("^api/airport/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
+    url("rating/$", "rating", name="rating"),
+    url("^api/airport/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/$", 
         "airports_for_boundary", name="airports_for_boundary"),
-    url("^api/flight/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/", 
+    url("^api/flight/list/(?P<south>.*)/(?P<west>.*)/(?P<north>.*)/(?P<east>.*)/$", 
         "flights_for_boundary", name="flights_for_boundary"),
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 )
